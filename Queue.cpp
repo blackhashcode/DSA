a560080d62d07338de700c9a017c001767cd1012
blackhashcode <mushybro7@gmail.com>
2025-05-22 23:47:04 +0600

    Queue cpp

---
+++ Queue.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+#define SIZE 1000
+
+class Queue{
+    int *arr;
+    int capacity;
+    int front;
+    int rear;
+    int count;
+    
+    public:
+        Queue(int size = SIZE);
+        ~Queue();
+        
+        int dequeue();
+        void enqueue(int x);
+        int peek();
+        int size();
+        bool isEmpty();
+        bool isFull();
+};
+
+Queue::Queue(int size){
+    arr = new int[size];
+    capacity = size;
+    front = 0;
+    rear = -1;
+    count = 0;
+}
+
+Queue :: ~Queue(){
+    delete[] arr;
+}
+
+int Queue :: dequeue()
+{
+    if (isEmpty()){
+        cout<<"underflow \n";
+        return 0;
+    }
+    
+    int x = arr[front];
+    cout<<"remove"<<x<<endl;
+    
+    front = (front + 1) % capacity;
+    count--;
+    
+    return x;
+}
+
+void Queue :: enqueue(int item){
+    if(isFull()){
+        cout<<"overflow\n";
+        exit(0);
+    }
+    
+    cout <<"inseting "<< item <<endl;
+    
+    rear = (rear + 1) % capacity;
+    arr[rear] = item;
+    count++;
+}
+
+int Queue:: peek(){
+    if(isEmpty()){
+        cout<<"underflow \n";
+        exit(EXIT_FAILURE);
+    }
+    
+    return arr[front];
+}
+
+int Queue::size(){
+    return count;
+}
+
+bool Queue::isEmpty(){
+    return (size() == 0);
+}
+
+bool Queue::isFull(){
+    return(size()== capacity);
+}
+
+
+int main(){
+    Queue q(5);
+ 
+    q.enqueue(1);
+    q.enqueue(2);
+    q.enqueue(3);
+ 
+    cout << "The front element is " << q.peek() << endl;
+    q.dequeue();
+ 
+    q.enqueue(4);
+ 
+    cout << "The queue size is " << q.size() << endl;
+ 
+    q.dequeue();
+    q.dequeue();
+    q.dequeue();
+ 
+    if (q.isEmpty()) {
+        cout << "The queue is empty\n";
+    }
+    else {
+        cout << "The queue is not empty\n";
+    }
+}
